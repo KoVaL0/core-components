427643a710c4b8ad82bd5bdf975af038f881cbff
KoVaL0 <goks223300@gmail.com>
2022-11-02 12:39:08 +0300

    chore(*): settings rollup and workflows

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,37 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import typescript from '@rollup/plugin-typescript';
 
-const packageJson = require("./package.json");
+const pkg = require("./package.json");
+
+const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
 
 export default {
   input: "src/index.ts",
   output: [
     {
-      file: packageJson.main,
+      file: pkg.main,
       format: "cjs",
       sourcemap: true
     },
     {
-      file: packageJson.module,
+      file: pkg.module,
       format: "esm",
       sourcemap: true
     }
   ],
   plugins: [
-    peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: 'dist',
+      }),
     postcss({
         extensions: ['.css']
     })
-  ]
+  ],
+  external,
 };
